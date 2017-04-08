fd90a4e995dea4a68b8b95fa7b267109d8f4508d
Kryštof Tulinger <k.tulinger@seznam.cz>
2017-04-08 12:29:51 +0200

    enabling debugger for my local address

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,7 +6,7 @@ $configurator = new Nette\Configurator;
 
 define( "APP_DIR", __DIR__ );
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator -> setDebugMode('85.70.17.135'); // enable for your remote IP
 $configurator -> enableDebugger( __DIR__ . '/../log' );
 
 $configurator -> setTempDirectory( __DIR__ . '/../temp' );
